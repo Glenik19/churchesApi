d5c716a42efe98dbf1ceb65ba60b8224558d8abc
Glejdi <glenik19@htl-shkoder.com>
2025-02-26 15:07:30 +0100

    new design3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,50 @@
+<script>
+    import img from "$lib/assets/church.jpg";
+
+    console.log(img);
+
+</script>
+
 <style>
         .container {
-            background: white;
-            padding: 20px;
-            border-radius: 10px;
-            max-width: 600px;
-            margin: auto;
+        background: white;
+        padding: 20px;
+        border-radius: 10px;
+        max-width: 600px;
+        width: 100%;
+        margin: auto;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        min-height: 700px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
         }
+
         h1 {
             color: red;
         }
         .btn {
-            display: inline-block;
-            padding: 10px 20px;
-            font-size: 16px;
-            color: white;
-            background-color: #007BFF;
-            border: none;
-            border-radius: 5px;
-            text-decoration: none;
-            margin: 15px 0;
-            cursor: pointer;
-        }
+        display: inline-block;
+        padding: 10px 20px;
+        font-size: 16px;
+        color: white;
+        background-color: #007BFF;
+        border: none;
+        border-radius: 5px;
+        text-decoration: none;
+        margin: 15px 0;
+        cursor: pointer;
+    }
         .btn:hover {
             background-color: #0056b3;
         }
 </style>
 
-    <div class="container">
+    <div class="container" style="background-image: url({img});">
         <h1>CHURCHES IN ALBANIA</h1>
         <div>
 			
